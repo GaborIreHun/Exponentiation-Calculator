8942fd71e4cc1e9b02e433cacbc1848c0ae7c7cc
GaborIreHun <76515173+GaborIreHun@users.noreply.github.com>
2021-01-30 19:41:13 +0000

    Add files via upload

---
+++ Exponentiation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+class Exponentiation
+{
+    public int baseValue;
+    public int exponentValue;
+    public int result = 1;
+
+
+    public void SetBaseValue(int exponentiationBaseValue)
+    {
+        baseValue = exponentiationBaseValue;
+    }
+
+    public int GetBaseValue()
+    {
+        return baseValue;
+    }
+
+    public void SetExponentValue(int expontentiationExponentValue)
+    {
+        exponentValue = expontentiationExponentValue;
+    }
+
+    public int GetExponentValue()
+    {
+        return exponentValue;
+    }
+
+    public int IntegerPower()
+    {
+        
+        for (int i = 1; i<= exponentValue; ++i)
+        {
+            result *= baseValue;
+        }
+        return result;
+    }
+
+
+}
+
---
+++ ExponentiationTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class ExponentiationTest
+{
+    static void Main()
+    {
+        Exponentiation exponentiation = new Exponentiation();
+
+        Console.WriteLine("\n\t\t<<<<<<<<< Exponentiation calculator >>>>>>>>>\n\n");
+
+        Console.Write("Please enter the base value: ");
+        int exponentiationtheBaseValue = int.Parse(Console.ReadLine());
+        exponentiation.SetBaseValue(exponentiationtheBaseValue);
+
+        Console.Write("\nPlease enter the exponent value: ");
+        int exponentiationtheExponentValue = int.Parse(Console.ReadLine());
+        exponentiation.SetExponentValue(exponentiationtheExponentValue);
+
+        Console.WriteLine($"\n\nThe result for your calculation is: {exponentiation.IntegerPower()}");
+
+
+    }
+}
+
